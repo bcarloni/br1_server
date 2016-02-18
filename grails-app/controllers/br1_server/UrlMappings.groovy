6217faf3c3f4161a517b612e7b5580088855e120
bcarloni <bruno.carloni@mercadolibre.com>
2016-02-18 17:21:20 -0300

    urlMapping

--- grails-app/controllers/br1_server/UrlMappings.groovy
+++ grails-app/controllers/br1_server/UrlMappings.groovy
@@ -3,20 +3,19 @@ package br1_server
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+            "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
+             "/response"(controller:"response", parseRequest:true){
+                action = [GET:"index", POST:"postRefund", PUT:"notSupported", DELETE: "notSupported", OPTIONS:"notSupported"]
+                contrains {
+                }
+            }
+
+            "/"(view:"/index")
+                "500"(view:'/error')
+                "404"(view:'/notFound')
+            }
         }
-
-        "/response"(controller:"response", parseRequest:true){
-		action = [GET:"index", POST:"postRefund", PUT:"notSupported", DELETE: "notSupported", OPTIONS:"notSupported"]
-		contrains {
-		}	
-	}
-
-	"/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
-    }
 }
